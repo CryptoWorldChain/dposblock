fcf82731bf5565b218584be160c5f05c6fe45617
brew <brewcwv@gmail.com>
2018-07-14 10:53:02 +0800

    set dpos vote bugs

--- src/main/scala/org/brewchain/dposblk/action/PDDutyTermVote.scala
+++ src/main/scala/org/brewchain/dposblk/action/PDDutyTermVote.scala
@@ -68,7 +68,7 @@ object PDDutyTermVoteService extends LogHelper with PBUtils with LService[PSDuty
 
         DTask_DutyTermVote.synchronized({
           if ((StringUtils.isBlank(tm.getSign) ||StringUtils.equals(pbo.getBcuid, cn.getBcuid)) ||
-              (StringUtils.isBlank(vq.getSign) || vq.getTermId<pbo.getTermId||
+              (StringUtils.isBlank(vq.getSign) || vq.getTermId<=pbo.getTermId||//!!<=
                   StringUtils.equals(pbo.getBcuid, cn.getBcuid)) &&
             //            && StringUtils.isBlank(vq.getMessageId) || vq.getMessageId.equals(pbo.getLastTermUid))
             ((tm.getTermId <= pbo.getLastTermId) && tm.getTermId <= pbo.getTermId - 1
