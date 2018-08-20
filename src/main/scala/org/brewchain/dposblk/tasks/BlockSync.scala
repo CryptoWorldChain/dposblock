529ed2d251b855878343ad678acd99c79eecc1a3
sean <sean@finchain360.com>
2018-08-20 18:13:51 +0800

    rm logs

--- src/main/scala/org/brewchain/dposblk/tasks/BlockSync.scala
+++ src/main/scala/org/brewchain/dposblk/tasks/BlockSync.scala
@@ -30,7 +30,7 @@ object BlockSync extends LogHelper {
     //Scheduler.runManager(runable,0,100, TimeUnit.MILLISECONDS)
   }
   def trySyncBlock(block_max_maybe_wanted: Int, fastNodeID: String)(implicit network: Network): Unit = {
-    log.debug("syncblocklog --> block_max_maybe_wanted:" + block_max_maybe_wanted);
+//    log.debug("syncblocklog --> block_max_maybe_wanted:" + block_max_maybe_wanted);
     if (network.nodeByBcuid(fastNodeID) == network.noneNode) {
       log.debug("cannot sync log bcuid not found in dposnet:nodeid=" + fastNodeID + ":");
     } else {
@@ -38,7 +38,7 @@ object BlockSync extends LogHelper {
       //
       //      log.debug("try sync block: Max block= " + block_max_wanted + ",cur=" + cn.getCurBlock + ",running=" + running.get)
       try {
-        log.debug("syncblocklog --> try sync block: want max block= " + block_max_maybe_wanted + ",maxreqheight=" + maxReqHeight.get + ",cur=" + cn.getCurBlock + ",running=" + running.get)
+//        log.debug("syncblocklog --> try sync block: want max block= " + block_max_maybe_wanted + ",maxreqheight=" + maxReqHeight.get + ",cur=" + cn.getCurBlock + ",running=" + running.get)
         maxReqHeight.synchronized({
           if (maxReqHeight.get > block_max_maybe_wanted && cn.getCurBlock >= block_max_maybe_wanted) {
             log.debug("not need to sync block: Max block=" + block_max_maybe_wanted + ",maxreqheight=" + maxReqHeight.get + ",cur=" + cn.getCurBlock + ",running=" + running.get)
@@ -79,7 +79,7 @@ object BlockSync extends LogHelper {
 
           var cc = cn.getCurBlock + 1;
           while (cc <= block_max_wanted) {
-            log.debug("syncblocklog --> DTask_SyncBlock cc:"+ cc + " endidx:" + Math.min(cc + DConfig.SYNCBLK_PAGE_SIZE - 1, block_max_wanted))
+//            log.debug("syncblocklog --> DTask_SyncBlock cc:"+ cc + " endidx:" + Math.min(cc + DConfig.SYNCBLK_PAGE_SIZE - 1, block_max_wanted))
             val runner = DTask_SyncBlock(startIdx = cc, endIdx =
               Math.min(cc + DConfig.SYNCBLK_PAGE_SIZE - 1, block_max_wanted),
               network = network, fastNodeID, runCounter)
@@ -90,16 +90,16 @@ object BlockSync extends LogHelper {
                 runCounter.incrementAndGet();
                 Scheduler.runOnce(runner);
                 runed = true;
-                log.debug("syncblocklog --> sync task runned")
+//                log.debug("syncblocklog --> sync task runned")
               }
             })
             var lastLogTime = 0L;
-            log.debug("syncblocklog --> runCounter.get:" + runCounter.get + " DConfig.SYNCBLK_MAX_RUNNER:" + DConfig.SYNCBLK_MAX_RUNNER)
+//            log.debug("syncblocklog --> runCounter.get:" + runCounter.get + " DConfig.SYNCBLK_MAX_RUNNER:" + DConfig.SYNCBLK_MAX_RUNNER)
             while (runCounter.get >= DConfig.SYNCBLK_MAX_RUNNER) {
               //wait... for next runner
               try {
                 if (System.currentTimeMillis() - lastLogTime > 10 * 1000) {
-                  log.debug("syncblocklog --> waiting for runner:cur=" + runCounter.get + " MAX_RUNNER=" + DConfig.SYNCBLK_MAX_RUNNER)
+//                  log.debug("syncblocklog --> waiting for runner:cur=" + runCounter.get + " MAX_RUNNER=" + DConfig.SYNCBLK_MAX_RUNNER)
                   lastLogTime = System.currentTimeMillis()
                 }
                 this.synchronized(this.wait(DConfig.SYNCBLK_WAITSEC_NEXTRUN))
@@ -108,9 +108,9 @@ object BlockSync extends LogHelper {
                 case e: Throwable =>
               }
             }
-            log.debug("syncblocklog --> runCounter.get:" + runCounter.get)
+//            log.debug("syncblocklog --> runCounter.get:" + runCounter.get)
             if (!runed) {
-              log.debug("syncblocklog --> rerun sync task")
+//              log.debug("syncblocklog --> rerun sync task")
               runCounter.incrementAndGet();
               Scheduler.runOnce(runner);
             }
@@ -122,7 +122,7 @@ object BlockSync extends LogHelper {
             }
             this.synchronized(Thread.sleep(DConfig.SYNCBLK_WAITSEC_NEXTRUN))
           }
-          log.debug("syncblocklog --> finished init follow up logs:" + DCtrl.curDN().getCurBlock);
+//          log.debug("syncblocklog --> finished init follow up logs:" + DCtrl.curDN().getCurBlock);
         } else {
           log.debug("skip request follow up logs:" + DCtrl.curDN().getCurBlock + ",block_wanted=" + block_max_maybe_wanted
             + ",from=" + fastNodeID);
